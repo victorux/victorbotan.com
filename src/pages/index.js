d493d380b76e6733ba37f3cb759f8da4a950b722
Victor Botan <victorb@VICTORS-MBP.station>
2021-09-27 09:36:34 +0100

    Added the header section in indexjs file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,33 @@
 import * as React from "react"
 import herobg from "../../src/images/hero-bg.webp"
+import logo from "../images/logo.webp"
 
 const IndexPage = () => {
   return (
-    <header className="w-full h-screen wh-200 pt-12 bg-cover" style={{ backgroundImage: `url('${herobg}')`}} >
-      <div className="wrap">
-
-      {/* Navigation */}
-      <nav>
-          <div>logo</div>
-          <ul>
-            <li><a href="http://localhost:8000/">About</a></li>
-            <li><a href="http://localhost:8000/">Work</a></li>
-            <li><a href="http://localhost:8000/">hello@victorbotan.com</a></li>
-          </ul>
+    <header className="h-screen wh-200 pt-20 bg-cover bg-black flex flex-col" style={{ backgroundImage: `url('${herobg}')`}} >
+      <div className="container mx-auto px-10 flex-none">
+        {/* Navigation */}
+        <nav className="flex justify-between flex-nowrap flex-wrapw-full items-center">
+          <div className="z-20">
+            <a href="/" className="flex flex-row items-center">
+              <img src={logo} className=""></img>
+              <span className="text-white pl-5 text-xl tracking-wider">Victor Botan</span>
+            </a>
+          </div>
+          <div className="menu text-white text-xl z-20 flex gap-6 tracking-wide">
+            <a href="/">About</a>
+            <a href="/">Work</a>
+            <a href="/">hello@victorbotan.com</a>
+          </div>
         </nav>
-
-        {/* Hero Message */}
-        <div>
-          <h1>Hello, I’m Front End Developer</h1>
-          <p>I code beautifuly simple things</p>
-          <button className="py-4 px-4 bg-blue-600 text-white">Test Button</button>
-        </div>   
-      </div>    
+      </div> 
+      {/* Hero Message */}
+      <div className="container mx-auto mt-0  pb-20 pl-10 z-20 relative text-white text-xl flex flex-col place-items-start justify-center gap-6 flex-grow ">
+        <h1 className="text-6xl font-bold">Hello, I’m Front End Developer</h1>
+        <p>I code beautifuly simple things</p>
+        <button className="py-5 px-8 bg-pur hover:bg-pur-dark hover:shadow-md transition duration-480 ease-in-out z-10 relative rounded-2xl">See my work ↓ </button>
+      </div>   
+         
     </ header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage:  {
-        
+      fontFamily: {
+        pop: "'Poppins', sans-serif",
+      },
+      colors: {
+        pur: {
+          DEFAULT: '#5F3D7D',
+          dark: '#1B42D7',
+        }
       }
     },
   },
